bfb66206298329089061bba63711a43589ef6883
LongchuanYu <remly@qq.com>
2021-12-14 22:12:25 +0800

    init

---
+++ mall_common_service/src/test/java/com/mall/common_service/mall_common_service/MallCommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.common_service.mall_common_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCommonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall_common_web/src/test/java/com/mall/common_web/mall_common_web/MallCommonWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.common_web.mall_common_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCommonWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
